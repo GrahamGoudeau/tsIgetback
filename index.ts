cdd4cb444433092b2efe0b2459ba3732bf4eda79
Graham Goudeau <grahamgoudeau@gmail.com>
2016-11-28 02:27:41 -0500

    Adding root path

--- index.ts
+++ index.ts
@@ -17,13 +17,17 @@ if (process.env.PRODUCTION === 'true') {
     dbUrl = 'mongodb://localhost:27017/igetback-db';
 }
 mongoose.connect(dbUrl);
-utils.addSecureRoute(app, 'test', utils.HttpMethod.GET);
-
-const builder = new utils.RouteBuilder('/products', (t, x) => {return});
-app.set('port', process.env.PORT || 5000);
-app.get('/', (req, res) => res.send(4));
-app.get('/products', (req, res) => res.send('Got a request for products'));
-app.get('/reviews', (req, res) => res.send('Got a request for reviews'));
+const builder = new utils.RouteBuilder('/', (req, res) => {
+    console.log('responding...');
+    res.json('ok');
+});
+const newRoute = new utils.Route(builder);
+const manager = new utils.RouteManager(app);
+manager.addRoute(newRoute);
+//app.set('port', process.env.PORT || 5000);
+//app.get('/', (req, res) => res.send(4));
+//app.get('/products', (req, res) => res.send('Got a request for products'));
+//app.get('/reviews', (req, res) => res.send('Got a request for reviews'));
 
 const server = app.listen(8000, "localhost", () => {
     const {address, port} = server.address();
