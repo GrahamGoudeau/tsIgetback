0952f727d1ed98f265fec3c019670c48e279b3b4
Graham Goudeau <grahamgoudeau@gmail.com>
2016-11-29 11:24:23 -0500

    Add get with password method and change return type signatures

--- db.ts
+++ db.ts
@@ -4,27 +4,31 @@ import * as security from './security';
 import * as mongoose from 'mongoose';
 
 type IUser = models.IUser;
+type Either<L, R> = tsmonad.Either<L, R>;
+const Either = tsmonad.Either;
 
-export enum DatabaseErrorMessages {
+export enum DatabaseErrorMessage {
     USER_EXISTS,
     USER_NOT_FOUND
 }
 
+export type DatabaseResult<T> = Either<DatabaseErrorMessage, T>
+
 export class DatabaseError extends Error {
-    constructor(public message: string, public errorValue: DatabaseErrorMessages) {
+    constructor(public message: string, public errorValue: DatabaseErrorMessage) {
         super(message);
     }
 
     static userExists(): DatabaseError {
-        return new DatabaseError('user exists', DatabaseErrorMessages.USER_EXISTS);
+        return new DatabaseError('user exists', DatabaseErrorMessage.USER_EXISTS);
     }
 
     static userNotFound(): DatabaseError {
-        return new DatabaseError('user not found', DatabaseErrorMessages.USER_NOT_FOUND);
+        return new DatabaseError('user not found', DatabaseErrorMessage.USER_NOT_FOUND);
     }
 }
 
-export type DbResult<T> = tsmonad.Either<DatabaseError, T>
+export type DbResult<T> = Either<DatabaseError, T>
 
 export interface OneUserQuery {
     email: string
@@ -42,13 +46,13 @@ function hashPassword(email: string, password: string) {
     return security.hashPassword(email, password);
 }
 
-export async function createUser(firstName: string, lastName: string, email: string, password: string): Promise<IUser> {
+export async function createUser(firstName: string, lastName: string, email: string, password: string): Promise<DatabaseResult<IUser>> {
     const userQuery: OneUserQuery = {
         email: email
     };
     const numExistingResult: Number = await models.User.count(userQuery);
     if (numExistingResult != 0) {
-        throw DatabaseError.userExists();
+        return Either.left(DatabaseErrorMessage.USER_EXISTS);
     }
     const newUserDetails: IUser = {
         firstName: firstName,
@@ -62,20 +66,29 @@ export async function createUser(firstName: string, lastName: string, email: str
         tripsFromAirport: []
     };
     const newUser: models.IUserModel = new models.User(newUserDetails);
-    return await newUser.save();
+    return Either.right(await newUser.save());
 }
 
-async function getUserFromQuery(query: OneUserQuery): Promise<models.IUser> {
+export async function getUserFromEmail(query: OneUserQuery): Promise<DatabaseResult<IUser>> {
     const existingUser = await models.User.findOne(query);
     if (existingUser == null) {
-        throw DatabaseError.userNotFound();
+        return Either.left(DatabaseErrorMessage.USER_NOT_FOUND);
     }
 
-    return existingUser;
+    return Either.right(existingUser);
 }
 
-export async function getUser(query: OneUserQuery): Promise<models.IUser> {
-    return getUserFromQuery(query);
+export async function getUserFromEmailAndPassword(query: UserPasswordQuery): Promise<DatabaseResult<IUser>> {
+    const dbQuery = {
+        email: query.email,
+        passwordHash: security.hashPassword(query.email, query.password)
+    };
+    const user = await models.User.findOne(dbQuery);
+    if (user == null) {
+        return Either.left(DatabaseErrorMessage.USER_NOT_FOUND);
+    }
+
+    return Either.right(user);
 }
 
 async function getTrips(query: TripsQuery, model: mongoose.Model<models.ITripModel>): Promise<models.ITrip[]> {
