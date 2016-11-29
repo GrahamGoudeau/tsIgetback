551e6d5725a47469bade60ab0c1590f8560ff59f
Graham Goudeau <grahamgoudeau@gmail.com>
2016-11-29 11:25:56 -0500

    Add documentation to index.ts

--- index.ts
+++ index.ts
@@ -10,12 +10,14 @@ const app: express.Express = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const airportCodesFile: string = __dirname + '/data/airport-codes.dat';
-const collegesFile: string = __dirname + '/data/colleges.dat';
+// load data files into memory
+const airportCodesFile: string = `${__dirname}/data/airport-codes.dat`;
+const collegesFile: string = `${__dirname}/data/colleges.dat`;
 
 const airportCodes: string[] = utils.readLines(airportCodesFile);
 const colleges: string[] = utils.readLines(collegesFile);
 
+// connect to the db
 let dbUrl: string;
 if (process.env.PRODUCTION === 'true') {
     dbUrl = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@ds019886.mlab.com:19886/igetback-db';
@@ -24,34 +26,46 @@ if (process.env.PRODUCTION === 'true') {
 }
 mongoose.connect(dbUrl);
 
+// set up routes
 const routeManager = new utils.RouteManager(app);
 
+// route builders
 const indexBuilder = new utils.RouteBuilder('/', (req, res) => {
     console.log('responding...');
     res.json('ok');
 });
 
-const userCreateBuilder = new utils.RouteBuilder('/createUser', user.createUser)
+const userCreateBuilder = new utils.RouteBuilder('/api/createUser', user.handleCreateUser)
     .setIsAjax(true)
     .setIsSecure(false)
     .setHttpMethod(utils.HttpMethod.POST);
 
+const loginBuilder = new utils.RouteBuilder('/api/login', user.handleLogin)
+    .setIsAjax(true)
+    .setHttpMethod(utils.HttpMethod.POST);
+
+const secureTestBuilder = new utils.RouteBuilder('/api/secure', (req, res) => {
+    console.log('testing');
+    res.send('in secure');
+}).setIsAjax(true)
+  .setIsSecure(true);
+
+// construct and set routes
 const indexRoute = new utils.Route(indexBuilder);
 const createRoute = new utils.Route(userCreateBuilder);
+const loginRoute = new utils.Route(loginBuilder);
+const secureRoute = new utils.Route(secureTestBuilder);
 
 const routes: utils.Route[] = [
     indexRoute,
-    createRoute
+    createRoute,
+    loginRoute,
+    secureRoute,
 ];
 
 routeManager.addRoutes(routes);
 
-
-//app.set('port', process.env.PORT || 5000);
-//app.get('/', (req, res) => res.send(4));
-//app.get('/products', (req, res) => res.send('Got a request for products'));
-//app.get('/reviews', (req, res) => res.send('Got a request for reviews'));
-
+// run
 const server = app.listen(8000, "localhost", () => {
     const {address, port} = server.address();
     console.log('Listening on http://localhost:' + port);
