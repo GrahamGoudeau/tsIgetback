8fe661f0b46934343c8d94228a1d599318cf8515
Graham Goudeau <grahamgoudeau@gmail.com>
2016-12-24 17:11:59 -0600

    Fix date regex bug

--- server/api/utils.ts
+++ server/api/utils.ts
@@ -5,7 +5,7 @@ export function o<A, B, C>(f: (y: B) => C,
     return x => f(g(x));
 }
 
-export const dateRegex: RegExp = /^[0-9]{1, 2}\/[0-9]{1,2}\/[0-9]{1, 4}$/;
+export const dateRegex: RegExp = /^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{1,4}$/;
 
 export function readLines(filePath: string): string[] {
     return fs.readFileSync(filePath).toString().split('\n');
